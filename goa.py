d7528164d7bb94049d0d09af809552160e5ea969
Daren Jacobs <darenjacobs@Darens-MacBook-Pro-2.local>
2019-04-09 14:08:52 -0400

    updated goa.py

--- goa.py
+++ goa.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
 """Copyright 2019 tellic LLC. All rights reserved.
 
 Author: Daren Jacobs
 Created: 2019-03-28
-Last Modified: 2019-04-05
+Last Modified: 2019-04-09
 
 Jira Ticket: TELLIC-523 - ETL the OMIM, Gene Ontology, +1Data
 
-GOAL: Put the downloaded and extracted Gene Ontology gz files data in BigQuery
+GOAL: Put the Gene Ontology files in GCS
 
 Description:
 This script downloads .gz files from:
 http://current.geneontology.org/annotations/index.html
 and uploads the extracted files to:
 https://console.cloud.google.com/storage/browser/tellic-dev/geneontology
-and loads the data to:
+and lodads ddata to:
 https://console.cloud.google.com/bigquery?project=tellic-dev&organizationId=23262195837&p=tellic-dev&d=GeneOntology&t=GAF_files&page=table
 """
-
 
 import gzip
 import locale
@@ -60,7 +59,6 @@ def create_session(project, bucket_name):
     """Create GCS client"""
 
     LOGGER.info("Creating GCS client")
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'tellic-dev-2807ffb4dd7f.json'
     client = storage.Client(project=project)
     bucket = client.get_bucket(bucket_name)
 
@@ -94,54 +92,64 @@ def get_bucket_info(upload_bucket, subdir):
     return blob_list
 
 
-def copy_to_bucket(filename, upload_bucket, subdir, temp_dest):
+def copy_to_bucket(bucket, subdir, filename):
     """Copy extracted gz file to GCS"""
 
+    # upload to GCS
+    blob = bucket.blob(subdir + '/' + filename)
+    blob.upload_from_filename(filename)
+
+
+def get_bad_lines(temp_dest):
+    """ Get lines that stat with !"""
+    bad_lines = 0
+    with gzip.open(temp_dest) as f_in:
+        for line in f_in:
+            this_line = line.decode('utf-8')
+            if this_line[0] == '!':
+                bad_lines += 1
+                continue
+
+    return bad_lines
+
+
+def extract_gz_file(filename, temp_dest):
+    """Extact and return the gz file and number of lines
+    """
+
+    bad_lines = get_bad_lines(temp_dest)
     with gzip.open(temp_dest) as f_in:
         with open(filename, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
-            blob = upload_bucket.blob(subdir + '/' + filename)
-            blob.upload_from_filename(filename)
+            total_lines = len(open(filename).readlines())
+
+    total_lines = total_lines - bad_lines
+    LOGGER.debug("TOTAL LINES: %d", total_lines)
+    return total_lines, filename
 
 
-def get_total_lines(temp_dest):
-    """ Get total number of lines in the extracted gz file """
-    total_lines = 0
-    with gzip.open(temp_dest) as f_in:
-        f_temp = f_in.read()
-        total_lines = len(f_temp)
-        LOGGER.info("TOTAL LINES: %s", total_lines)
-        return total_lines
+def create_text_block(f_in, num_lines):
+    """Write extracted file to text file in chunks of num_lines"""
 
-
-def create_text_block(filename, temp_dest, num_lines):
-    """Write NUM_LINES number of lines to a text file
-       This makes it so the files are in blocks
-    """
-
-    # Write lines to file
+    this_file = 'text_block.txt'
+    scratch_file = open(this_file, 'w+')
     line_count = 0
-    this_file = 'text.' + filename
-    scratch_file = open(this_file, 'a')
-    with gzip.open(temp_dest) as f_in:
-        for line in f_in:
-            if line_count > num_lines:
-                scratch_file.close()
-                break
 
-            this_line = line.decode('utf-8')
-            if this_line[0] == '!':
-                continue
+    for line in f_in:
+        if line_count > num_lines:
+            break
 
-            scratch_file.write(this_line)
-            line_count += 1
+        if line[0] == '!':
+            continue
 
+        scratch_file.write(line)
+        line_count += 1
 
     return this_file
 
 
 def load_lines(file_name):
-    """ write the text block to BigQuery table """
+    """Load text block into Big Query"""
     LOGGER.info("Writing text block to BigQuery")
 
     # create article CSV
@@ -152,36 +160,42 @@ def load_lines(file_name):
                     'db_object_type', 'taxon', 'date', 'assigned_by',
                     'annotation_extension']
     dataframe = pd.read_csv(csv_file, delimiter='\t', index_col=False,
-                            names=column_names, verbose=True)
+                            names=column_names, error_bad_lines=False,
+                            warn_bad_lines=True, skip_blank_lines=True,
+                            verbose=True, dtype={'annotation_extension': str})
+
     dataframe.to_csv('new.csv', encoding='utf-8', index=False)
-    dataframe.to_gbq(destination_table=DESTINATION_TABLE, project_id=PROJECT,
-                     if_exists='append')
+    dataframe.to_gbq(destination_table=DESTINATION_TABLE, project_id=PROJECT, if_exists='append')
+
+    os.remove('new.csv')
 
 
 @timer
-def main(url, num_lines):
+def main():
     """
     Create a GCS client and get a bucket object,
     Download and extract the .gz file from the URL,
     Check if if the file exists in GCS
     and delete local temp files
     """
+    # Set url
+    url = GOA_URL
 
     # Set a client session and get the bucket
     bucket = create_session(PROJECT, BUCKET_NAME)
 
-    my_dir = SUB_DIR
     filename = url.split('/')[-1]
+    LOGGER.debug("FILE NAME: %s", filename)
 
-    # Output path for the extacted file
+    # Temp location for gz file
     temp_dest = tempfile.mkdtemp('_go') + '/' + filename
+    my_dir = SUB_DIR
     blob_list = get_bucket_info(bucket, my_dir)
-    LOGGER.info("HERE IS THE BLOB_LIST: %s", blob_list)
+    LOGGER.info("GCS Bucket BLOB_LIST: %s", blob_list)
 
-    # check if gz file is in GCS
+    # check if file is in GCS
     if my_dir + '/' + filename[:-3] in blob_list:
-        LOGGER.info("File already exists, skipping download: %s",
-                    filename[:-3])
+        LOGGER.info("File already exists, skipping download: %s", filename[:-3])
 
     # Download the gz file
     else:
@@ -189,35 +203,48 @@ def main(url, num_lines):
         urllib.request.urlcleanup()
         urllib.request.urlretrieve(url, temp_dest)
 
-        # Copy gz file to GCS
-        copy_to_bucket(filename[:-3], bucket, my_dir, temp_dest)
+        # Get extracted file & total number of lines in extracted file
+        total_lines, extracted_file = extract_gz_file(filename[:-3], temp_dest)
 
-        # Get total number of lines
-        total_lines = get_total_lines(temp_dest)
+        # GCS Copy
+        copy_to_bucket(bucket, my_dir, extracted_file)
 
-        # create while loop that writes total number of lines are written
+        # Set up while loop
+        if total_lines < NUM_LINES:
+            num_lines = total_lines
+        else:
+            num_lines = NUM_LINES
+
         lines_written = 0
         to_go = 0
+        locale.setlocale(locale.LC_ALL, 'en_US') # I want the numbers to look nice
 
-        # I want the countdown numbers to look nice
-        locale.setlocale(locale.LC_ALL, 'en_US.utf8')
-        while lines_written < total_lines:
-            current_file = create_text_block(filename[:-3], temp_dest,
-                                             num_lines)
-            lines_written += len(open(current_file).readlines())
-            load_lines(current_file)
-            to_go = total_lines - lines_written
-            countdown = locale.format_string("%d", to_go, grouping=True)
-            LOGGER.info("%d lines of %d written\n %s lines to go",
-                        lines_written, total_lines, countdown)
-            os.remove(current_file)
+        # create while-loop that writes extracted file in chunks
+        with open(extracted_file) as f_in:
+            while lines_written < total_lines:
+                to_go = total_lines - lines_written
+                if to_go < num_lines:
+                    num_lines = to_go
+
+                current_file = create_text_block(f_in, num_lines)
+                LOGGER.debug("CURRENT_FILE: %s", current_file)
+                lines_written += len(open(current_file).readlines())
+                load_lines(current_file)
+
+                countdown = locale.format_string("%d", to_go, grouping=True)
+                LOGGER.info("%d lines of %d written\n %s lines to go",
+                            lines_written, total_lines, countdown)
+                os.remove(current_file)
+
 
         os.remove(temp_dest)
+        os.remove(extracted_file)
+
 
 
 if __name__ == '__main__':
 
-    # List of gz files to process
+    ## List of gz files to process
     # http://current.geneontology.org/annotations/goa_human.gaf.gz
     # http://current.geneontology.org/annotations/goa_human_complex.gaf.gz
     # http://current.geneontology.org/annotations/goa_human_isoform.gaf.gz
@@ -230,6 +257,6 @@ if __name__ == '__main__':
     DATASET = 'GeneOntology'
     TABLE = 'GAF_files'
     DESTINATION_TABLE = (DATASET + '.' + TABLE)
-    NUM_LINES = 999999
+    NUM_LINES = 50000
 
-    main(GOA_URL, NUM_LINES)
+    main()
